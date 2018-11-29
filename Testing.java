5634aba061c21e027e06d1fbbcfe10dc57c0a4c7
melvinrajendran <44681827+melvinrajendran@users.noreply.github.com>
2018-11-29 12:16:43 -0500

    Created method convertToInteger
    Created method convertToInteger, which  takes in an ArrayList<Integer> and converts the list of elements into a single integer value.  That integer is then returned.

--- Testing.java
+++ Testing.java
@@ -1,8 +1,19 @@
-public class Testing{
-	public static void main(String [] args){
-	System.out.println("Nikitha");
-	System.out.println("Melvin");
-	System.out.println("yo");
-	System.out.println("Blue");
+import java.util.ArrayList;
+
+public class Testing {
+
+	public static void main(String [] args) {
+
 	}
+
+	public static int convertToInteger (ArrayList<Integer> list) {
+		String term = "";
+
+		for (int i = 0; i < list.size(); i++) {
+			term += list.get(i);
+		}
+
+		return Integer.parseInt(term);
+	}
+
 }
