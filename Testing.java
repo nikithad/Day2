89228260258b54da1a5bb0b3f4802dc3afe7af64
nikithad <45398944+nikithad@users.noreply.github.com>
2018-11-29 12:30:09 -0500

    final prac
    final prac

--- Testing.java
+++ Testing.java
@@ -3,15 +3,25 @@ import java.util.ArrayList;
 public class Testing {
 
 	public static void main(String [] args) {
+	ArrayList<Integer> lis = new ArrayList<>();
+	lis.add(1);
+	lis.add(2);
+	lis.add(3);
 
+	int z = 123;
+	System.out.println(convertToArrayList(z));
+
+	System.out.println(convertToInteger(lis));
 	}
 
 	public static ArrayList<Integer> convertToArrayList (int n) {
 		ArrayList<Integer> list = new ArrayList<>();
-		for (int i = 0; i < n.length; i ++){
-			list.add(i);
+		while (n!=0){
+			int x = n%10;
+			list.add(0, x);
+			 n = n/10;
 		}
-		return n;
+		return list;
 	}
 
 	public static int convertToInteger (ArrayList<Integer> list) {
